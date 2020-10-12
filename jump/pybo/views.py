2ae6edaf41898598cc2d3040b373de0770a0df1f
seongbin <apple0192@gmail.com>
2020-10-12 12:54:20 +0800

    'questions_list_in_pybo'

--- jump/pybo/views.py
+++ jump/pybo/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse('안녕하세요 pybo에 오신 것을 환영합니다.')
+    """
+    pybo 목록 출력
+    """
+    quesion_list = Question. objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybno/question_list.html', context)
