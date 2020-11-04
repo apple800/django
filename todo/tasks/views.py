f495f61381b4d0dbf7bc8197216634af34bb3508
seongbin <apple0192@gmail.com>
2020-11-04 18:19:18 +0800

    'model_form'

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import *
+from .forms import *
 
 # Create your views here.
+
+
 def index(request):
     tasks = Task.objects.all()
 
+    form = TaskForm()
+
     context = {
-        'tasks' : tasks
+        'tasks': tasks,
+        'form': form,
     }
     return render(request, 'tasks/list.html', context)
